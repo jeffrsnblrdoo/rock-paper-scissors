8c711db4fdf29eeb7c11163e3266580b6d2617fd
Jefferson Belardo <118502737+jeffrsnblrdoo@users.noreply.github.com>
2025-01-13 20:59:05 +0800

    Add play round

--- javascript.js
+++ javascript.js
@@ -10,15 +10,25 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function getHumanChoice() {
   let choice = prompt("rock, paper, or scissors?");
+  choice = choice.toLowerCase();
   return choice;
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock!");
+  }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
