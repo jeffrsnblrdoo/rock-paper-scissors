7d67e737d32fb9817e58db98c74e0b8d0edf2c28
Jefferson Belardo <iamjeffersonbelardo@gmail.com>
2025-01-20 21:06:19 +0800

    Add score display

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
 let rounds = 5;
-let humanChoice = "";
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3 + 1);
@@ -16,11 +15,13 @@ function getComputerChoice() {
 }
 
 const result = document.querySelector("#result");
-
 const buttons = document.querySelectorAll("button");
+const playerScore = document.querySelector("#player-score");
+const compScore = document.querySelector("#computer-score");
+
 buttons.forEach((button) =>
     button.addEventListener("click", (e) => {
-        humanChoice = e.target.textContent;
+        let humanChoice = e.target.textContent;
 
         if (rounds != 0) {
             playRound(humanChoice, getComputerChoice());
@@ -42,21 +43,27 @@ function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock" && computerChoice === "paper") {
         result.textContent = "You lose! Paper beats Rock!";
         computerScore++;
+        compScore.textContent = `Computer score: ${computerScore}`;
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
         result.textContent = "You lose! Scissors beats Paper!";
         computerScore++;
+        compScore.textContent = `Computer score: ${computerScore}`;
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
         result.textContent = "You lose! Rock beats Scissors!";
         computerScore++;
+        compScore.textContent = `Computer score: ${computerScore}`;
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
         result.textContent = "You win! Rock beats Scissors!";
         humanScore++;
+        playerScore.textContent = `Player score: ${humanScore}`;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         result.textContent = "You win! Paper beats Rock!";
         humanScore++;
+        playerScore.textContent = `Player score: ${humanScore}`;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         result.textContent = "You win! Scissors beats Paper!";
         humanScore++;
+        playerScore.textContent = `Player score: ${humanScore}`;
     } else {
         result.textContent = "Draw!";
     }
